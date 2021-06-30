e97e5839cde1a76778d99c6540ea5ecb26b26b89
Peter <phellwege1@gmail.com>
2021-06-29 18:37:42 -0600

    bug squash 2

--- server/server.js
+++ server/server.js
@@ -12,11 +12,18 @@ const jwt = require("jsonwebtoken");
 // require("./config/mongoose.config")(db_name);
 
 const app = express();
+const payload = {
+    id: user._id
+};
 
+// notice that we're using the SECRET_KEY from our .env file
+const userToken = jwt.sign(payload, process.env.SECRET_KEY);
 // req.body undefined without this!
+
+
 app.use(express.json());
 app.use(cors());
-app.use(express.urlencoded({ extended: true })); 
+app.use(express.urlencoded({ extended: true }));
 
 
 app.listen(port, () =>
