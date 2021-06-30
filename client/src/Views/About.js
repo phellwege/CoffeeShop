9de0249cdbf82da56c9a4e03c00478303417d916
Peter <phellwege1@gmail.com>
2021-06-29 18:10:26 -0600

    bug squash

--- client/src/Views/About.js
+++ client/src/Views/About.js
@@ -4,14 +4,13 @@ import { Link } from '@reach/router'
 import MapContainer from '../Components/GoogleMaps'
 import HeadPanel from '../Components/HeadPanel'
 function About() {
-
-
     return(
 <div>
     <div>
-        <HeadPanel/>
+        <HeadPanel/><Link to="/">Home</Link>
     </div>
-    <Link to="/">Home</Link>
+        <div class="about_wrapper">
+    
     <div>
         <h2>Who we are</h2>
         <p>We are a coffee shop who happen to also have an e-commerce site specifically dedicated to coffee.</p>
@@ -26,9 +25,12 @@ function About() {
         <h4>phone</h4>
         <h4>address</h4>
     </div>
+    <div>
         <div class="google_maps">
         <MapContainer />
         </div>
+    </div>
+    </div>
 </div>
 )}
 export default About;
