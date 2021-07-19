bf83a429e1aa722f53ea43d307ffb30d00dba8a6
Peter <phellwege1@gmail.com>
2021-07-19 11:53:49 -0600

    removed paypal altogether

--- client/src/Components/PaymentProcessor.js
+++ client/src/Components/PaymentProcessor.js
@@ -3,35 +3,9 @@ import axios from 'axios';
 import { Link } from '@reach/router'
 
 function paymentProcessor() {
-    <html>
-        <body>
-            <script
-                src="https://www.paypal.com/sdk/js?client-id=YOUR_CLIENT_ID"> // Replace YOUR_CLIENT_ID with your sandbox client ID
-            </script>
-
-            <div id="paypal-button-container"></div>
-
-            <script>
-                paypal.Buttons({
-                    createOrder: function(data, actions) {
-          return actions.order.create({
-                    purchase_units: [{
-                    amount: {
-                    value: '0.01'
-              }
-            }]
-          });
-        },
-                onApprove: function(data, actions) {
-          return actions.order.capture().then(function(details) {
-                    alert('Transaction completed by ' + details.payer.name.given_name);
-          });
-        }
-      }).render('#paypal-button-container'); // Display payment options on your web page
-            </script>
-        </body>
-    </html>
-
-
-}
+    return(
+      <div>
+        
+      </div>
+    )}
 export default paymentProcessor;
