ba282c60a6339a212fc5dfe2803298a41d3f1d66
Peter <phellwege1@gmail.com>
2021-07-19 17:00:55 -0600

    fixed db

--- server/server.js
+++ server/server.js
@@ -1,44 +1,19 @@
+require("dotenv").config();
 
-const cookieParser = require('cookie-parser');
-const express = require("express");
-const cors = require("cors");
-const port = 8000;
-const db_name = "ecommerce_db";
-require('dotenv').config();
+const express = require("express"),
+    cookieParser = require("cookie-parser"),
+    cors = require("cors");
+
 require("./config/mongoose.config")(process.env.DB_NAME);
-const jwt = require("jsonwebtoken");
-
-// mongoose.config could be changed to export a function that lets us pass in a db_name like we pass app into the function that our routes file exports
-// const db_name = "my_db";
-// require("./config/mongoose.config")(db_name);
 
 const app = express();
-// const payload = {
-//     id: user._id
-// };
 
-const server = app.listen(process.env.DB_PORT, () =>
-    console.log(`Listening on port ${process.env.DB_PORT}`)
-);
-
-
-// notice that we're using the SECRET_KEY from our .env file
-// const userToken = jwt.sign(payload, process.env.SECRET_KEY);
-// req.body undefined without this!
-
-
-
-
-// res.cookie("mycookie", "mydata", { httpOnly: true }).json({
-//     message: "This response has a cookie"
-// });
-
-app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
-app.use(express.urlencoded({ credentials: true, extended: true, origin:"http://localhost:3000" }));
+app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
+app.use(express.json());
+
 require("./routes/user.routes")(app);
 
-app.listen(port, () =>
-    console.log(`Listening on port ${port} for REQuests to RESpond to.`)
+app.listen(process.env.DB_PORT, () =>
+    console.log(`Listening on port ${process.env.DB_PORT}`)
 );
